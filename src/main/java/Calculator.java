4dad28e2148b94f450642140d7f956cc443c6539
박범진 <qjawlsqjacks@naver.com>
2021-12-10 11:06:05 +0900

    refactor: 문자열 계산기 조건 변경, 테스트 코드 변경

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,40 +4,89 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Calculator {
+    String[] operation = {"+", "-", "*", "/"};
 
     public int calculate(String input) {
-        if(isBlank(input)) throw new RuntimeException();
-        return splitAndCalulate(input);
-    }
-
-    private boolean isBlank(String input) {
-        return input == null || input.isEmpty();
-    }
-    
-    private int splitAndCalulate(String input) {
-        int start = 0;
-        Deque<Integer> number = new ArrayDeque<>();
-        Queue<Character> operation = new LinkedList<>();
-        for (int i = 0; i < input.length(); i++) {
-            char now = input.charAt(i);
-            if(now == '*' || now == '+' || now == '-' || now == '/') {
-                number.addLast(Integer.parseInt(input.substring(start, i)));
-                start = i + 1;
-                operation.add(now);
-            }
-        }
-        number.addLast(Integer.parseInt(input.substring(start)));
-        
+        isBlank(input);
+        NumbersAndOperation numbersAndOperation = splitNumbersOperation(validation(split(input)));
+        Deque<Integer> number = numbersAndOperation.number;
+        Queue<String> operations = numbersAndOperation.operation;
         while(number.size() > 1) {
             int first = number.pollFirst();
             int second = number.pollFirst();
-            char oper = operation.poll();
-            if(oper == '+') number.addFirst(first + second);
-            if(oper == '*') number.addFirst(first * second);
-            if(oper == '-') number.addFirst(first - second);
-            if(oper == '/') number.addFirst(first / second);
+            String oper = operations.poll();
+            if(oper.equals(operation[0])) number.addFirst(plus(first, second));
+            if(oper.equals(operation[1])) number.addFirst(minus(first, second));
+            if(oper.equals(operation[2])) number.addFirst(multiply(first, second));
+            if(oper.equals(operation[3])) number.addFirst(division(first, second));
         }
         return number.poll();
     }
 
+    private Integer plus(int first, int second){
+        return first + second;
+    }
+
+    private Integer minus(int first, int second){
+        return first - second;
+    }
+
+    private Integer multiply(int first, int second){
+        return first * second;
+    }
+
+    private Integer division(int first, int second){
+        return first / second;
+    }
+
+    private void isBlank(String input) {
+        if(input == null || input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private String[] split(String input) {
+        return input.split(" ");
+    }
+
+    private String[] validation(String[] input) {
+        for (int i = 0; i < input.length; i++) {
+            boolean flag;
+            if(i % 2 == 0) {
+                flag = false;
+                for (int j = 0; j < operation.length; j++) {
+                    if(input[i].equals(operation[j])) flag = true;
+                 }
+            } else {
+                flag = true;
+                for (int j = 0; j < operation.length; j++) {
+                    if(input[i].equals(operation[j])) flag = false;
+                }
+            }
+            if(flag) throw new IllegalArgumentException();
+        }
+        return input;
+    }
+    
+    private NumbersAndOperation splitNumbersOperation(String input[]) {
+        Deque<Integer> number = new ArrayDeque<>();
+        Queue<String> operation = new LinkedList<>();
+        for (int i = 0; i < input.length; i++) {
+            String now = input[i];
+            if(i % 2 == 0) number.addLast(Integer.parseInt(now));
+            else operation.add(now);
+        }
+
+        return new NumbersAndOperation(number, operation);
+    }
+
+    static class NumbersAndOperation {
+        Deque<Integer> number;
+        Queue<String> operation;
+
+        public NumbersAndOperation(Deque<Integer> number, Queue<String> operation) {
+            this.number = number;
+            this.operation = operation;
+        }
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,43 +2,69 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.lang.reflect.Method;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class CalculatorTest {
 
     Calculator calculator = new Calculator();
+    Calculator.NumbersAndOperation numbersAndOperation;
+    String inputNull;
+    String inputEmpty;
+    String input;
 
-    @Test
-    void calculate() {
-        String input = "2+3*4/2";
-        String inputBlank = "";
-        assertThat(calculator.calculate(input)).isEqualTo(10);
-        assertThatThrownBy(() -> {
-            calculator.calculate(inputBlank);
-        }).isInstanceOf(RuntimeException.class);
+    @BeforeEach
+    void setUp() {
+        inputNull = null;
+        inputEmpty = " ";
+        input = "2 + 3 * 4 / 2";
     }
 
     @Test
+    void calculate() {
+        assertThat(calculator.calculate(input)).isEqualTo(10);
+        assertThatThrownBy(() -> calculator.calculate(inputNull)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
     @DisplayName("입력 값이 공백인지 확인")
-    void CheckInputIsBlankOrIsEmpty() throws Exception {
-        String textNull = null;
-        String textEmpty = "";
-        String text = "abc";
+    @NullAndEmptySource
+    void IsBlankOrIsEmpty(String input) throws Exception {
         Method method = calculator.getClass().getDeclaredMethod("isBlank", String.class);
         method.setAccessible(true);
-        assertThat((boolean) method.invoke(calculator, textNull)).isTrue();
-        assertThat((boolean) method.invoke(calculator, textEmpty)).isTrue();
-        assertThat((boolean) method.invoke(calculator, text)).isFalse();
+        assertThatThrownBy(() -> method.invoke(calculator, input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력 값 공백 기준으로 나누기")
+    void split() throws Exception {
+        Method method = calculator.getClass().getDeclaredMethod("split", String.class);
+        method.setAccessible(true);
+        String[] result = (String[]) method.invoke(calculator, input);
+        assertThat(result.length).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("올바른 입력값인지 확인")
+    void validation() throws Exception {
+        String[] input = {"2", "2", "+"};
+        Method method = calculator.getClass().getDeclaredMethod("validation", String[].class);
+        method.setAccessible(true);
+        assertThatThrownBy(() -> method.invoke(calculator, input)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("입력 값 전처리 후 계산")
     void splitAndCalculate() throws Exception {
-        String input = "2+3*4/2";
-        Method method = calculator.getClass().getDeclaredMethod("splitAndCalulate", String.class);
+        String[] inpu = {"2", "+", "3", "*", "4", "/", "2"};
+        Method method = calculator.getClass().getDeclaredMethod("splitNumbersOperation", String[].class);
         method.setAccessible(true);
-        assertThat((int) method.invoke(calculator, input)).isEqualTo(10);
+        numbersAndOperation = (Calculator.NumbersAndOperation) method.invoke(calculator, (Object) inpu);
+        assertThat(numbersAndOperation.number.size()).isEqualTo(4);
+        assertThat(numbersAndOperation.operation.size()).isEqualTo(3);
     }
 
 
