d3843eb09742d80718684939eaf9ba9dbfae5fe0
박범진 <qjawlsqjacks@fasoo.com>
2022-03-31 17:43:52 +0900

    feat: 입력값 자리수 확인 기능

--- src/main/java/baseball/NumberBaseBallGame.java
+++ src/main/java/baseball/NumberBaseBallGame.java
@@ -1,5 +1,24 @@
 package baseball;
 
-public class NumberBaseBallGame {
+import java.util.Random;
 
+public class NumberBaseBallGame {
+	
+	
+	private int makeAnswer() {
+		return makeNumber(10) * 100 + makeNumber(10) * 10 + makeNumber(10);
+	}
+	
+	private int makeNumber(int number) {
+		Random random = new Random();
+		return random.nextInt(1, number);
+	}
+
+	
+	public boolean checkLengthThree(int input) {
+		if (input < 101 || input > 999) {
+			throw new RuntimeException("3자리가 아닙니다.");
+		}
+		return true;
+	}
 }
--- src/test/java/baseball/NumberBaseBallGameTest.java
+++ src/test/java/baseball/NumberBaseBallGameTest.java
@@ -1,5 +1,21 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class NumberBaseBallGameTest {
 
+	NumberBaseBallGame game = new NumberBaseBallGame();
+	
+	@Test
+	public void 입력값이_3자리인지_확인() {
+		assertThat(game.checkLengthThree(123)).isTrue();
+		assertThatExceptionOfType(RuntimeException.class)
+			.isThrownBy(() -> game.checkLengthThree(1000));
+	}
 }
